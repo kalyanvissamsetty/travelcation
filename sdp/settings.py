09a38a4422ecca0b2e52d562bc05022aec6fb969
Vissamsetty Kalyan <vissamsettykalyan@gmail.com>
2021-05-22 23:56:27 +0530

    Update settings.py

--- sdp/settings.py
+++ sdp/settings.py
@@ -43,7 +43,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNosieMiddleware'
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'sdp.urls'
